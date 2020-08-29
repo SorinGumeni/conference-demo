c2b4206b0831fcb3effc475d32527cc9c7170706
Sorin Gumeni <gumeni.sorin@yahoo.com>
2020-08-29 13:27:56 +0100

    Initial commit

---
+++ src/main/java/com/demo/conferencedemo/config/PersistenceConfiguration.java
@@ -0,0 +1,16 @@
+package com.demo.conferencedemo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PersistenceConfiguration {
+//    @Bean
+//    public DataSource dataSource() {
+//        DataSourceBuilder builder = DataSourceBuilder.create();
+//        builder.url("jdbc:postgresql://172.17.0.2:5432/conference_app");
+//        builder.password("Welcome");
+//        builder.username("postgres");
+//        System.out.println("My data source bean has been initialized and set");
+//        return builder.build();
+//    }
+}
